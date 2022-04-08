293ec2c24c25958e64db28cc846ec9f2c598cea4
unknown <jainprajwal123@gmail.com>
2022-04-08 18:53:16 +0530

    feat: button stories

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,40 +1,36 @@
 import React from 'react';
+import {Button} from "../components/Button/Button"
 
-import { Button } from './Button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-};
+  title: "Button",
+  component: Button
+}
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+export const PrimaryButton = Template.bind({});
+export const SecondaryButton = Template.bind({});
+export const SuccessButton = Template.bind({});
+export const WarningButton = Template.bind({});
+export const DangerButton = Template.bind({});
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
+PrimaryButton.args = {
+  type: "primary"
+}
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+SecondaryButton.args = {
+  type: "secondary"
+}
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+SuccessButton.args = {
+  type: "success"
+}
+
+WarningButton.args = {
+  type: "warning"
+}
+
+DangerButton.args = {
+  type: "danger"
+}
