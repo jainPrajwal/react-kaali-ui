759d28784213e6c99752b0e12e1acc3a8ae8aecf
unknown <jainprajwal123@gmail.com>
2022-06-23 22:52:32 +0530

    feat: ripple effect

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,59 @@
-import React from "react";
-export const Button = ({ type }) => {
+import React, { useState, useEffect } from "react";
+import "./Button.css";
+export const Button = ({
+  type,
+  cssButton = {},
+  onClickHandler,
+  isDisabled = false,
+  children,
+}) => {
+  const [coords, setCoords] = useState({
+    x: -1,
+    y: -1,
+  });
+
+  const [isRippling, setIsRippling] = useState(false);
+
+  useEffect(() => {
+    if (coords.x !== -1 && coords.y !== -1) {
+      setIsRippling(true);
+      setTimeout(() => setIsRippling(false), 300);
+    } else setIsRippling(false);
+  }, [coords]);
+
+  useEffect(() => {
+    if (!isRippling) setCoords({ x: -1, y: -1 });
+  }, [isRippling]);
+
   return (
-   
-      <button class={`btn btn-${type}`}>{`${type}`} button</button>
-    
+    <button
+      className={`btn btn-${type} overflow-hidden`}
+      onClick={(e) => {
+        const rectangle = e.target.getBoundingClientRect();
+        setCoords({
+          x: e.clientX - rectangle.left,
+          y: e.clientY - rectangle.top,
+        });
+        typeof onClickHandler === `function` && onClickHandler();
+      }}
+      style={{
+        position: `relative`,
+        ...cssButton,
+      }}
+      disabled={isDisabled}
+    >
+      {isRippling ? (
+        <span
+          className="ripple"
+          style={{
+            left: coords.x,
+            top: coords.y,
+          }}
+        />
+      ) : (
+        ""
+      )}
+      <span className="content">{children}</span>
+    </button>
   );
 };
