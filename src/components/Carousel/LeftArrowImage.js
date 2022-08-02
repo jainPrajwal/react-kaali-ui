5e3b30b9b244f0faaddc2c98a7f2e08738f31984
unknown <jainprajwal123@gmail.com>
2022-08-02 21:27:28 +0530

    fix: carousel left and right image

--- src/components/Carousel/LeftArrowImage.js
+++ src/components/Carousel/LeftArrowImage.js
@@ -2,7 +2,7 @@ import React from "react";
 const LeftArrowImage = () => {
   return (
     <img
-      src="https://github.com/j836/kaaliUI/blob/main/assets/images/left-arrow.png?raw=true"
+      src="https://res.cloudinary.com/dmk11fqw8/image/upload/v1659455669/left-arrow_rl8sol.png"
       alt="Previous Arrow"
       className="d-block"
     />
--- src/components/Carousel/RightArrowImage.js
+++ src/components/Carousel/RightArrowImage.js
@@ -2,8 +2,8 @@ import React from "react";
 const RightArrowImage = () => {
   return (
     <img
-      src="https://github.com/j836/kaaliUI/blob/main/assets/images/right-arrow.png?raw=true"
-      alt="Previous Arrow"
+      src="https://res.cloudinary.com/dmk11fqw8/image/upload/v1659455720/right-arrow_yirsqu.png"
+      alt="Next Arrow"
       className="d-block"
     />
   );
